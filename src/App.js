deae65bf6b0f212bf11792e18b0d5fccb5d7b28d
ahmedavdihodzic <100534892+ahmedavdihodzic@users.noreply.github.com>
2023-03-07 16:53:56 +0100

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './App.css'
+import './index.css'
+
+import{Brand,Cta,Navbar} from './components';
+import{Footer, Blog, Possibility, Features, WhatGPT3, Header} from './container';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar/>
+        <Header/>
+      </div>
+      <Brand/>
+      <WhatGPT3/>
+      <Features/>
+      <Possibility/>
+      <Cta/>
+      <Blog/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/cta/Cta.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './cta.css';
+
+const Cta = () => {
+  return (
+    <div className="gtp3__cta">
+      <div className="gtp3__cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className="gtp3__cta-btn">
+        <button type='button'>Get Started</button>
+      </div>
+    </div>
+  )
+}
+
+export default  Cta;
